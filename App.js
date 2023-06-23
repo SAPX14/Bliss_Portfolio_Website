ccb6e0de7e15dd58d956a3b1166debcd72c0fd91
SAPX14 <80125084+SAPX14@users.noreply.github.com>
2023-06-23 19:58:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { AboutUs, FindUs, Footer, Gallery, Header, Intro, Menu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <Menu />
+    <Intro />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
