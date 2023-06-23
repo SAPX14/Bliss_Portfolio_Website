a99f7b6094c8ec386266fe5ebe55cc7cf0e9c443
SAPX14 <80125084+SAPX14@users.noreply.github.com>
2023-06-23 19:59:59 +0530

    Add files via upload

---
+++ constants/images.js
@@ -0,0 +1,27 @@
+import logo from '../assets/logo.png';
+import logo1 from '../assets/logo1.jpg';
+import spoon from '../assets/spoon.svg';
+import caribbean_panini from '../assets/caribbean_panini.png';
+import blast from '../assets/blast.png';
+import bomb_roll from '../assets/bomb_roll.png';
+import chilli_cheese_gb from '../assets/chilli_cheese_gb.png';
+import jini from '../assets/jini.png';
+import garlic_bread from '../assets/garlic_bread.png';
+import lays_mayo from '../assets/lays_mayo.png';
+import pahadi from '../assets/pahadi.png';
+import pizza_dosa from '../assets/pizza_dosa.png';
+
+export default {
+    logo,
+    logo1,
+    spoon,
+    caribbean_panini,
+    blast,
+    bomb_roll,
+    chilli_cheese_gb,
+    jini,
+    garlic_bread,
+    lays_mayo,
+    pahadi,
+    pizza_dosa,
+}
